c0e0049de6efe0e90dd7984d1f6a22b46e7f6ccf
che2n3jigw <che2n3jigw@163.com>
2026-01-19 17:30:31 +0800

    feat: Initial project setup
    Initialize the Android application "TouchSimulator".
    
    This commit introduces the basic file and directory structure for a new Android project, generated by Android Studio. It includes:
    - Gradle wrapper and build configuration files (`build.gradle.kts`, `settings.gradle.kts`, `gradle.properties`).
    - Application module (`app`) with a manifest, build script, and default resources.
    - Standard `.gitignore` files for Android and IDEA.
    - Default ProGuard rules and resource files, including launcher icons.
    - Configuration for Android Gradle Plugin `8.13.2` and Kotlin `2.3.0`.
    - The application is configured with a `minSdk` of 24 and `compileSdk` of 36, using Java 11.
    
    (cherry picked from commit 5c68f700966535c0a7e006d359f9ab70efacada9)

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.che2n3jigw.touchsimulator"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.che2n3jigw.touchsimulator"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+}
+
+dependencies {
+}
